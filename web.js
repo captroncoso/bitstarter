634b51431fe1c59dd1520c0568236753e0b9ad1a
captroncoso <jacobo.garcia.perez@gmail.com>
2013-07-15 10:40:24 +0000

    Added file

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var text = fs.readFileSync('index.html','utf8');
+  var buffer = new Buffer(text);
+  response.send(buffer.toString('utf8'));
+
 });
 
 var port = process.env.PORT || 5000;
