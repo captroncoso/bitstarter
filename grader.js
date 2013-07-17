0d79abbb382ae9a066ad29b3e986150c662075e8
captroncoso <jacobo.garcia.perez@gmail.com>
2013-07-17 07:43:00 +0000

    Added file

--- grader.js
+++ grader.js
@@ -20,12 +20,19 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
-
+var restl = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://fierce-reaches-1073.herokuapp.com";
+
+var loadUrl = function(URL) {
+  var res = restl.get(URL).toString();
+  return res;
+
+}; 
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -64,7 +71,9 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+         
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_url_file>', 'link to url', clone(loadUrl), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
